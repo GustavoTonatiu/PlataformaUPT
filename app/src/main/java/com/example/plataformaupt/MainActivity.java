a51e982e5732ac9ae3351b820c3f3071aee56780
Tonatiu Tenorio <gustavotonatl.g@hotmail.com>
2020-05-07 18:50:51 -0500

    CREACION GIT

--- app/src/main/java/com/example/plataformaupt/MainActivity.java
+++ app/src/main/java/com/example/plataformaupt/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.plataformaupt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView regt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        regt=(TextView)findViewById(R.id.registroLogin);
+        regt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(),Registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/plataformaupt/Registro.java
+++ app/src/main/java/com/example/plataformaupt/Registro.java
@@ -2,13 +2,25 @@ package com.example.plataformaupt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Registro extends AppCompatActivity {
+    private TextView inicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        inicio=(TextView)findViewById(R.id.regresar);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
